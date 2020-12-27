b3045d2024d3f3358c64f6dc8430c2445cf61b84
Christian Landgren <christian@landgren.nu>
2020-12-27 10:37:21 +0100

    refactored to using generators

--- packages/app/components/login.component.js
+++ packages/app/components/login.component.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
-import { Platform } from 'react-native'
-import { SafeAreaView, StyleSheet, Image, Linking, KeyboardAvoidingView, TouchableOpacity, View } from 'react-native'
+import { Platform, SafeAreaView, StyleSheet, Image, Linking, KeyboardAvoidingView, TouchableOpacity, View } from 'react-native'
+
 import { Button, Icon, Modal, Card, Text, ImageBackground, Divider, Layout, TopNavigation, Input } from '@ui-kitten/components'
 import Personnummer from 'personnummer'
-import {useAsyncStorage} from 'use-async-storage'
+import { useAsyncStorage } from 'use-async-storage'
 import { ScrollView } from 'react-native-gesture-handler'
-import {api} from '../lib/backend'
+import { api } from '../lib/backend'
 
 const baseUrl = 'https://api.skolplattformen.org'
 const funArguments = ['öppna', 'roliga', 'fungerande', 'billiga', 'snabba', 'fria', 'efterlängtade', 'coolare', 'första', 'upplysta', 'hemmagjorda', 'bättre', 'rebelliska', 'enkla', 'operfekta', 'fantastiska', 'agila'] // TODO: add moare
@@ -35,13 +35,13 @@ export const Login = ({ navigation, route }) => {
 
   const SecureIcon = (style) => (
     <Icon {...style} name='keypad-outline' />
-  ) 
+  )
   const CheckIcon = (style) => (
     <Icon {...style} name='checkmark-outline' />
-  ) 
+  )
   const LogoutIcon = (style) => (
     <Icon {...style} name='close-outline' />
-  ) 
+  )
   const PersonIcon = (style) => (
     <Icon {...style} name='person-outline' />
   )
@@ -60,9 +60,9 @@ export const Login = ({ navigation, route }) => {
 
   const openBankId = (token) => {
     try {
-      const bankIdUrl = Platform.OS === 'ios' ? `https://app.bankid.com/?autostarttoken=${token.token}&redirect=null` : `bankid:///?autostarttoken=${token.token}&redirect=null`  
+      const bankIdUrl = Platform.OS === 'ios' ? `https://app.bankid.com/?autostarttoken=${token.token}&redirect=null` : `bankid:///?autostarttoken=${token.token}&redirect=null`
       Linking.openURL(bankIdUrl)
-    } catch(err){ 
+    } catch (err) {
       setHasBankId(false)
       setError('Öppna BankID manuellt')
     }
@@ -73,10 +73,10 @@ export const Login = ({ navigation, route }) => {
     console.log('requesting login for', socialSecurityNumber)
     const loginStatus = await api.login(socialSecurityNumber)
     openBankId(loginStatus.token) // TODO: verify this solution after issue https://github.com/kolplattformen/embedded-api/issues/3 is resolved
-    loginStatus.on("PENDING", () => console.log("BankID app not yet opened"))
-    loginStatus.on("USER_SIGN", () => console.log("BankID app is open"))
-    loginStatus.on("ERROR", () => setError('Inloggningen misslyckades, försök igen!') && setVisible(false))
-    loginStatus.on("OK", async () => {
+    loginStatus.on('PENDING', () => console.log('BankID app not yet opened'))
+    loginStatus.on('USER_SIGN', () => console.log('BankID app is open'))
+    loginStatus.on('ERROR', () => setError('Inloggningen misslyckades, försök igen!') && setVisible(false))
+    loginStatus.on('OK', async () => {
       setLoggedIn(true)
       const session = api.getSessionCookie()
       setCookie(session)
@@ -94,69 +94,76 @@ export const Login = ({ navigation, route }) => {
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
-      <TopNavigation title={`Skolplattformen.org - det ${argument} alternativet`} alignment='center'/>
-      {loggedIn ? <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-
-        <Image source={require('../assets/man.png')} style={{maxHeight: 300, width: '100%', borderBottomWidth:1 }}></Image>
-        <View style={{ marginTop: 80, justifyContent: 'flex-start', alignItems: 'flex-start', flex: 1}}>
-          <Text category="h4">{socialSecurityNumber}</Text>
-          <Text>{error ? error : 'Hurra, du är inloggad!'}</Text>
-          <Button
-            status="success"
-            size="medium"
-            style={{marginTop: 10, width: 200}}
-            accessoryRight = {CheckIcon}
-            onPress={() => navigateToChildren()}>
-            {error ? 'Försök igen' : 'Fortsätt'}
-          </Button>
-          <Button 
-            onPress={() => logout()}
-            accessoryRight={LogoutIcon}
-            style={{marginTop: 10, width: 200}}
-            size="medium">
-            Logga ut
-          </Button>
-        </View>
-      </Layout>
-    : <KeyboardAvoidingView>
-        <Layout style={{ flex: 1 }}>
-          { 
-            // hidden easter egg, just touch the image to login without bankId if you still have a valid token
-          }
-          <TouchableOpacity onPress={navigateToChildren} style={{height: 320}}> 
-            <Image source={require('../assets/boys.png')} style={{height: 320, marginTop: -20, marginLeft: -10, width: '110%'}}></Image>
-          </TouchableOpacity>
-          <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', paddingHorizontal: 20}}>
-            <Input label='Personnummer' autoFocus={true} value={socialSecurityNumber}
-              style={{minHeight:70}}
-                accessoryLeft = {PersonIcon}
-                keyboardType='numeric'
-                caption={error && error.message || ''}
-                onChangeText = {text => handleInput(text)}
-                placeholder="Ditt personnr (10 eller 12 siffror)"/>
-            <Button onPress={startLogin} style={{width: "100%"}} 
-              appearence='ghost' 
-              disabled={!valid}
-              status='primary'
-              accessoryRight={SecureIcon}
-              size='medium'>
-              Öppna BankID
+      <TopNavigation title={`Skolplattformen.org - det ${argument} alternativet`} alignment='center' />
+      {loggedIn
+        ? <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Image source={require('../assets/man.png')} style={{ maxHeight: 300, width: '100%', borderBottomWidth: 1 }} />
+          <View style={{ marginTop: 80, justifyContent: 'flex-start', alignItems: 'flex-start', flex: 1 }}>
+            <Text category='h4'>{socialSecurityNumber}</Text>
+            <Text>{error || 'Hurra, du är inloggad!'}</Text>
+            <Button
+              status='success'
+              size='medium'
+              style={{ marginTop: 10, width: 200 }}
+              accessoryRight={CheckIcon}
+              onPress={() => navigateToChildren()}
+            >
+              {error ? 'Försök igen' : 'Fortsätt'}
+            </Button>
+            <Button
+              onPress={() => logout()}
+              accessoryRight={LogoutIcon}
+              style={{ marginTop: 10, width: 200 }}
+              size='medium'
+            >
+              Logga ut
             </Button>
           </View>
-        </Layout>
-      </KeyboardAvoidingView>
-    }
-        <Modal
+          </Layout>
+        : <KeyboardAvoidingView>
+          <Layout style={{ flex: 1 }}>
+            {
+            // hidden easter egg, just touch the image to login without bankId if you still have a valid token
+          }
+            <TouchableOpacity onPress={navigateToChildren} style={{ height: 320 }}>
+              <Image source={require('../assets/boys.png')} style={{ height: 320, marginTop: -20, marginLeft: -10, width: '110%' }} />
+            </TouchableOpacity>
+            <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start', paddingHorizontal: 20 }}>
+              <Input
+                label='Personnummer' autoFocus value={socialSecurityNumber}
+                style={{ minHeight: 70 }}
+                accessoryLeft={PersonIcon}
+                keyboardType='numeric'
+                caption={error && error.message || ''}
+                onChangeText={text => handleInput(text)}
+                placeholder='Ditt personnr (10 eller 12 siffror)'
+              />
+              <Button
+                onPress={startLogin} style={{ width: '100%' }}
+                appearence='ghost'
+                disabled={!valid}
+                status='primary'
+                accessoryRight={SecureIcon}
+                size='medium'
+              >
+                Öppna BankID
+              </Button>
+            </View>
+          </Layout>
+          </KeyboardAvoidingView>}
+      <Modal
         visible={visible}
         style={styles.modal}
         backdropStyle={styles.backdrop}
-        onBackdropPress={() => setVisible(false)}>
-        <Card disabled={true}>
-          {hasBankId ? <Text style={{margin: 10}}>Öppnar BankID. Växla tillbaka hit sen.</Text> : <Text style={{margin: 10}}>Väntar på BankID...</Text>}
-          
-          <Button 
+        onBackdropPress={() => setVisible(false)}
+      >
+        <Card disabled>
+          {hasBankId ? <Text style={{ margin: 10 }}>Öppnar BankID. Växla tillbaka hit sen.</Text> : <Text style={{ margin: 10 }}>Väntar på BankID...</Text>}
+
+          <Button
             visible={!loggedIn}
-            onPress={() => setVisible(false)}>
+            onPress={() => setVisible(false)}
+          >
             Avbryt
           </Button>
         </Card>
@@ -167,12 +174,12 @@ export const Login = ({ navigation, route }) => {
 
 const styles = StyleSheet.create({
   container: {
-    minHeight: 192,
+    minHeight: 192
   },
   modal: {
-    width: "80%"
+    width: '80%'
   },
   backdrop: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+  }
 })
--- packages/app/lib/backend.js
+++ packages/app/lib/backend.js
@@ -1,14 +1,22 @@
-import moment from 'moment'
-import init from "@skolplattformen/embedded-api"
-export const api = init(fetch)  // keep a static version of this object so we can keep the session alive
+import init from '@skolplattformen/embedded-api'
+export const api = init(fetch) // keep a static version of this object so we can keep the session alive
 
-export const loadChildrenDetails = async (children, what = {news: true}) => await Promise.all(children.map(async child => ({
-  ...child,
-  news: !what.news ? child.news : await api.getNews(child).catch(err => [{err}]),
-  calendar: !what.calendar ? child.calendar : await api.getCalendar(child).catch(err => [{err}]),
-  notifications:  !what.notifications ? child.notifications : await api.getNotifications(child).catch(err => [{err}]),
-  schedule: !what.schedule ? child.schedule : await api.getSchedule(child, moment().startOf('day'), moment().add(7,'days').endOf('day')).catch(err => [{err}]),
-  classmates: !what.classmates ? child.classmates : await api.getClassmates(child).catch(err => [{err}]),
-  menu: !what.menu ? child.menu : await api.getMenu(child).catch(err => [{err}]),
-})))
+export function * fillChild (child) {
+  console.log(`loading notifications for ${child.name}...`)
+  yield api.getNotifications(child).catch(err => Promise.resolve([{ err }])).then(notifications => ({ ...child, notifications }))
 
+  console.log(`loading news for ${child.name}...`)
+  yield api.getNews(child).catch(err => Promise.resolve([{ err }])).then(news => ({ ...child, news }))
+
+  console.log(`loading menu for ${child.name}...`)
+  yield api.getMenu(child).catch(err => Promise.resolve([{ err }])).then(menu => ({ ...child, menu }))
+
+  console.log(`loading calendar for ${child.name}...`)
+  yield api.getCalendar(child).catch(err => Promise.resolve([{ err }])).then(calendar => ({ ...child, calendar }))
+
+  console.log(`loading schedule for ${child.name}...`)
+  yield api.getSchedule(child).catch(err => Promise.resolve([{ err }])).then(schedule => ({ ...child, schedule }))
+
+  console.log(`loading classmates for ${child.name}...`)
+  yield api.getClassmates(child).catch(err => Promise.resolve([{ err }])).then(classmates => ({ ...child, classmates }))
+}
