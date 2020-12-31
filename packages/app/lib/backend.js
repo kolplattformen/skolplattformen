b2e097dfade7ca7403c5db401d199345f8735ab2
Christian Landgren <christian@landgren.nu>
2020-12-31 01:05:33 +0100

    do not emit old values which might overwrite new vales

--- packages/app/lib/backend.js
+++ packages/app/lib/backend.js
@@ -1,34 +1,34 @@
 import init from '@skolplattformen/embedded-api'
 export const api = init(fetch) // keep a static version of this object so we can keep the session alive
 
-export function* fillChild(child) {
+export function * fillChild (child) {
   console.log(`loading notifications for ${child.name}...`)
   yield api.getNotifications(child)
     .catch(err => Promise.resolve([{ err }]))
-    .then(notifications => ({ ...child, notifications }))
+    .then(notifications => ({ notifications }))
 
   console.log(`loading news for ${child.name}...`)
   yield api.getNews(child)
     .catch(err => Promise.resolve([{ err }]))
-    .then(news => ({ ...child, news }))
+    .then(news => ({ news }))
 
   console.log(`loading menu for ${child.name}...`)
   yield api.getMenu(child)
     .catch(err => Promise.resolve([{ err }]))
-    .then(menu => ({ ...child, menu }))
+    .then(menu => ({ menu }))
 
   console.log(`loading calendar for ${child.name}...`)
   yield api.getCalendar(child)
     .catch(err => Promise.resolve([{ err }]))
-    .then(calendar => ({ ...child, calendar }))
+    .then(calendar => ({ calendar }))
 
   console.log(`loading schedule for ${child.name}...`)
   yield api.getSchedule(child)
     .catch(err => Promise.resolve([{ err }]))
-    .then(schedule => ({ ...child, schedule }))
+    .then(schedule => ({ schedule }))
 
   console.log(`loading classmates for ${child.name}...`)
   yield api.getClassmates(child)
     .catch(err => Promise.resolve([{ err }]))
-    .then(classmates => ({ ...child, classmates }))
+    .then(classmates => ({ classmates }))
 }
