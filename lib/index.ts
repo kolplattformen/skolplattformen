8351ef275f1ac87da00fdeaa86a3f4e17a1e15b1
Johan Öbrink <johan.obrink@gmail.com>
2021-02-04 11:15:50 +0100

    feat: 🎸 Exporting all types in index (#45)

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,9 @@ import { Api } from './api'
 import { FetcherOptions } from './fetcher'
 import { AsyncishFunction, Fetch } from './types'
 
+export { Api, FetcherOptions }
+export * from './types'
+
 export default function init(fetch: Fetch, clearCookies: AsyncishFunction, options?: FetcherOptions): Api {
   return new Api(fetch, clearCookies, options)
 }
