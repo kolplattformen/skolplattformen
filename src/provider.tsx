32615ee504eac98d82f00a6e36c60284033dca37
Johan Öbrink <johan.obrink@gmail.com>
2021-02-06 21:22:34 +0100

    docs: ✏️ Added license and readme

--- src/provider.tsx
+++ src/provider.tsx
@@ -1,13 +1,16 @@
-import { Api } from '@skolplattformen/embedded-api';
-import React, { FC, PropsWithChildren, useEffect, useState } from 'react'
+/* eslint-disable react/prop-types */
+/* eslint-disable import/prefer-default-export */
+import { Api } from '@skolplattformen/embedded-api'
+import React, {
+  FC, PropsWithChildren, useEffect, useState,
+} from 'react'
 import { Provider } from 'react-redux'
 import { ApiContext } from './context'
 import store from './store'
-import { AsyncStorage, IApiContext } from './types';
+import { AsyncStorage, IApiContext } from './types'
 
 type TApiProvider = FC<PropsWithChildren<{ api: Api, storage: AsyncStorage }>>
 export const ApiProvider: TApiProvider = ({ children, api, storage }) => {
-
   const [isLoggedIn, setIsLoggedIn] = useState(api.isLoggedIn)
   const [isFake, setIsFake] = useState(api.isFake)
 
