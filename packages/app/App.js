2d16ad4228d8c51ab21b70fea433e9b48bcf7b2a
Johan Öbrink <johan.obrink@gmail.com>
2021-02-08 13:03:56 +0100

    Added Analytics for crash free reports

--- packages/app/App.js
+++ packages/app/App.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import {ApplicationProvider, IconRegistry} from '@ui-kitten/components'
-import {EvaIconsPack} from '@ui-kitten/eva-icons'
+import React, { useEffect } from 'react'
+import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import * as eva from '@eva-design/eva'
 import customization from './design/customization.json'
-import {AppNavigator} from './components/navigation.component'
+import { AppNavigator } from './components/navigation.component'
 import init from '@skolplattformen/embedded-api'
-import {ApiProvider} from '@skolplattformen/api-hooks'
+import { ApiProvider } from '@skolplattformen/api-hooks'
 import CookieManager from '@react-native-community/cookies'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import crashlytics from '@react-native-firebase/crashlytics'
 
 const api = init(fetch, () => {
   CookieManager.clearAll()
 })
 
 export default () => {
+  useEffect(() => {
+    crashlytics().log('App mounted')
+  }, [])
   return (
     <ApiProvider api={api} storage={AsyncStorage}>
       <IconRegistry icons={EvaIconsPack} />
-      <ApplicationProvider {...eva} theme={{...eva.light, ...customization}}>
+      <ApplicationProvider {...eva} theme={{ ...eva.light, ...customization }}>
         <AppNavigator />
       </ApplicationProvider>
     </ApiProvider>
