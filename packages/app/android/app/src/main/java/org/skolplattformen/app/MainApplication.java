0e20719c55b1a9456f2920bc1dc13ce5bc860634
Johan Öbrink <johan.obrink@gmail.com>
2021-02-08 12:15:26 +0100

    Enabled MultiDex

--- packages/app/android/app/src/main/java/org/skolplattformen/app/MainApplication.java
+++ packages/app/android/app/src/main/java/org/skolplattformen/app/MainApplication.java
@@ -1,6 +1,5 @@
 package org.skolplattformen.app;
 
-import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -10,8 +9,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.multidex.MultiDexApplication;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
