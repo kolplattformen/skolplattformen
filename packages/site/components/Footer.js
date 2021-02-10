0b74c5c5b168962780be404099e2f8713afea1bd
Rickard Natt och Dag <rickard@hey.com>
2021-02-10 18:40:21 +0100

    refactor: clean-up footer

--- packages/site/components/Footer.js
+++ packages/site/components/Footer.js
@@ -1,55 +1,24 @@
-import footerBg from '../assets/img/footer-bg.png'
 import footerLogo from '../assets/img/logo.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footerbg">
-        <img src={footerBg} className="svg" alt="" />
-      </div>
       <div className="footer-top pt-120 pb-110">
         <div className="container">
           <div className="row">
             <div className="col-lg-3 col-sm-6">
               <div className="footer-widget">
                 <div className="footer-logo">
-                  <a href="index.html">
-                    <img src={footerLogo} alt="" />
-                  </a>
+                  <img src={footerLogo} alt="" />
                 </div>
                 <p>
                   Skolplattformen utvecklas av föräldrar för föräldrar. Vill du
                   hjälpa till? Kom till vår{' '}
                   <a href="https://github.com/kolplattformen">Github</a>, där
                   finns all källkod och även uppgifter att ta tag i, vi behöver
-                  hjälp med allt från illustrationer, UX, Design och
+                  hjälp med allt från illustrationer, UX, design och
                   programmering. Vi har även en Discord där vi hjälps åt.
                 </p>
-
-                <div className="footer-social-area">
-                  <ul className="social-icons social-icons-light nav">
-                    <li>
-                      <a href="#" target="_blank">
-                        <i className="fa fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" target="_blank">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" target="_blank">
-                        <i className="fa fa-google-plus"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" target="_blank">
-                        <i className="fa fa-linkedin"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
                 <p>
                   <a href="mailto:info@skolplattformen.org">
                     info@skolplattformen.org
@@ -68,7 +37,7 @@ const Footer = () => {
                   <ul className="address-list">
                     <li>
                       <span>
-                        <i className="fa  fa-twitter"></i>
+                        <i className="fa fa-twitter"></i>
                       </span>
                       <a href="https://twitter.com/@landgren">
                         Christian Landgren
@@ -76,7 +45,7 @@ const Footer = () => {
                     </li>
                     <li>
                       <span>
-                        <i className="fa  fa-twitter"></i>
+                        <i className="fa fa-twitter"></i>
                       </span>
                       <a href="https://twitter.com/@erikhellman">
                         Erik Hellman
@@ -84,7 +53,7 @@ const Footer = () => {
                     </li>
                     <li>
                       <span>
-                        <i className="fa  fa-twitter"></i>
+                        <i className="fa fa-twitter"></i>
                       </span>
                       <a href="https://twitter.com/@johanobrink">
                         Johan Öbrink
@@ -121,12 +90,12 @@ const Footer = () => {
             <div className="col-lg-3 col-sm-6">
               <div className="footer-widget">
                 <div className="widget-body">
-                  <div className="twetter-post-inner">
+                  <div className="twitter-post-inner">
                     <div className="footer-post-details">
                       @iteam1337 Digitalisering på riktigt. <br />
                       <a href="https://iteam.se">https://iteam.se</a>
                     </div>
-                    <div className="twetter-post">
+                    <div className="twitter-post">
                       <span>
                         <i className="fa fa-twitter"></i>
                       </span>
