bc2f220f8e2382f51c3f0f77035b0329b284ee24
Johannes Larsson <johannes.larsson@protonmail.com>
2021-02-11 11:51:17 +0100

    Add a simple media links section
    I don't have much experience with React but I hope this can at least
    provide a nice starting point.
    I'm not sure which links to include but that's easy to change of course.
    
    I don't have kids or live in Stockholm but I thought this was a cool
    project :)

---
+++ packages/site/components/Media.js
@@ -0,0 +1,46 @@
+import { Col, Container, Row } from 'react-bootstrap'
+
+const MEDIA_DATA = [
+  {
+    "title": "Efter skandalerna – kodande föräldrar släpper egen skolapp",
+    "source": "Ny Teknik",
+    "link": "https://www.nyteknik.se/premium/efter-skandalerna-kodande-foraldrar-slapper-egen-skolapp-7009234",
+  }, {
+    "title": "Stockholms stad utreder föräldrarnas app: ”Kan vara olaglig”",
+    "source": "Ny Teknik",
+    "link": "https://www.nyteknik.se/digitalisering/stockholms-stad-utreder-foraldrarnas-app-kan-vara-olaglig-7009287",
+  }, {
+    "title": "Lärare dömer ut Skolplattformen",
+    "source": "Dagens Nyheter",
+    "link": "https://www.dn.se/sthlm/larare-domer-ut-skolplattformen/",
+  }, {
+    "title": "Han utmanar Skolplattformen med egen app: ”Något behövde göras”",
+    "source": "SVT Nyheter",
+    "link": "https://www.svt.se/nyheter/lokalt/stockholm/han-trottnade-pa-skolplattformen-byggde-en-egen-app",
+  },
+
+]
+
+const Media = () => {
+  return (
+    <section className="border-top pt-120 pb-80">
+      <Container>
+        <div class='section-title text-center'>
+          <h1>Öppna Skolplattformen i media</h1>
+        </div>
+        <Row>
+          {MEDIA_DATA.map((article, index) => (
+            <Col md={3} sm={6} key={`article-${index}`}>
+              <div>
+                <p><b><a href={article.link}>{article.title}</a></b></p>
+                <p>{article.source}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default Media
