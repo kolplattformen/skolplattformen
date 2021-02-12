524a11843c8ff430be508daf0976f5a655698dae
Jacob Klapwijk <jacob.klapwijk@mentimeter.com>
2021-02-12 14:22:25 +0100

    feat: add Open Graph title, image and description

--- packages/site/components/Layout.js
+++ packages/site/components/Layout.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import { Link as ScrollLink } from 'react-scroll'
 import favImg from '../assets/img/favicon.png'
+import logo from '../assets/img/logo.png'
 import { GA_TRACKING_ID } from './gtag'
 
 const Layout = (props) => {
@@ -26,6 +27,12 @@ const Layout = (props) => {
     <div>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Öppna skolplattformen" />
+        <meta
+          property="og:description"
+          content="Öppna Skolplattformen är en app för iOS och Android som gör det enklare för föräldrar att komma åt uppgifter i Skolplattformen."
+        />
+        <meta property="og:image" content={logo} />
         <title>{props.pageTitle}</title>
         <link rel="shortcut icon" type="image/png" href={favImg} />
         <link
