fb8b2071bfea7232dd26489f127c0c3c965bd4c7
Rickard Natt och Dag <rickard@hey.com>
2021-02-16 17:14:11 +0100

    chore: lägg till css purge

--- packages/site/tailwind.config.js
+++ packages/site/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
