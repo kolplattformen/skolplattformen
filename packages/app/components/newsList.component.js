5fb55875b8c30191c1847461575c1b1ff8c57345
Johan Öbrink <johan.obrink@gmail.com>
2021-02-17 21:19:18 +0100

    feat: 🎸 Dra och ladda om
    Genom att dra ner listorna anropas reload

--- packages/app/components/newsList.component.js
+++ packages/app/components/newsList.component.js
@@ -1,14 +1,27 @@
+import { useNews, useNewsDetails } from '@skolplattformen/api-hooks'
 import { List } from '@ui-kitten/components'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
+import { useChild } from './childContext.component'
 import { NewsListItem } from './newsListItem.component'
 
-export const NewsList = ({ news }) => {
+export const NewsList = () => {
+  const child = useChild()
+  const { data, status, reload } = useNews(child)
+  const [refreshing, setRefreshing] = useState(status === 'loading')
+  useEffect(() => {
+    setRefreshing(status === 'loading')
+  }, [status])
+
+  const refresh = () => reload()
+
   return (
     <List
+      onRefresh={refresh}
+      refreshing={refreshing}
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
-      data={news}
+      data={data}
       renderItem={(info) => (
         <NewsListItem key={info.item.id} item={info.item} />
       )}
--- packages/app/components/notificationsList.component.js
+++ packages/app/components/notificationsList.component.js
@@ -1,14 +1,26 @@
+import { useNotifications } from '@skolplattformen/api-hooks'
 import { List } from '@ui-kitten/components'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet } from 'react-native'
+import { useChild } from './childContext.component'
 import { Notification } from './notification.component'
 
-export const NotificationsList = ({ notifications }) => {
+export const NotificationsList = () => {
+  const child = useChild()
+  const { data, status, reload } = useNotifications(child)
+  const [refreshing, setRefreshing] = useState(status === 'loading')
+  useEffect(() => {
+    setRefreshing(status === 'loading')
+  }, [status])
+
+  const refresh = () => reload()
   return (
     <List
+      onRefresh={refresh}
+      refreshing={refreshing}
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
-      data={notifications}
+      data={data}
       renderItem={(info) => (
         <Notification key={info.item.id} item={info.item} />
       )}
