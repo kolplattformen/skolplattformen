069648a66b0e41f6fed77083c90b58a1e14382df
Rickard Natt och Dag <rickard@hey.com>
2021-02-18 16:59:32 +0100

    chore: ta bort console.log

--- packages/app/components/childContext.component.js
+++ packages/app/components/childContext.component.js
@@ -3,7 +3,6 @@ import React, { createContext, useContext } from 'react'
 export const ChildContext = createContext({})
 
 export const ChildProvider = ({ child, children }) => {
-  console.log('Provider', child)
   return <ChildContext.Provider value={child}>{children}</ChildContext.Provider>
 }
 
