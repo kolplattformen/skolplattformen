e78c88236961de593c267163ea0f85a442d30bbb
Rickard Natt och Dag <rickard@hey.com>
2021-03-15 07:37:13 +0100

    chore: update flash styling

--- packages/site/components/Flash.js
+++ packages/site/components/Flash.js
@@ -1,12 +1,27 @@
 import message from '../content/flash.json'
 
 const Flash = () => (
-  <div className="bg-pink-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
-    <div className="flex">
-      <div className="py-1"><svg className="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
-      <div>
-        <p className="font-bold">{message.subject}</p>
-        <p className="text-sm">{message.body}</p>
+  <div className="max-w-6xl mx-auto mt-4">
+    <div className="bg-pink-100 sm:rounded px-4 py-3 shadow-md" role="alert">
+      <div className="flex space-x-4">
+        <svg
+          className="w-6 h-6 mt-1"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <div className="flex-1">
+          <p className="font-bold">{message.subject}</p>
+          <p className="text-sm">{message.body}</p>
+        </div>
       </div>
     </div>
   </div>
