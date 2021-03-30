a55ac31389d9228d254e713afdcb90ec45620fb6
Viktor Sarström <viktorsarstrom@gmail.com>
2021-03-30 13:45:39 +0200

    Changed name from O to Ö

--- packages/site/components/Footer.js
+++ packages/site/components/Footer.js
@@ -6,7 +6,7 @@ const team = [
   { name: 'Erik Hellman', twitter: 'erikhellman' },
   { name: 'Johan Öbrink', twitter: 'johanobrink' },
   { name: 'Rickard Natt och Dag', twitter: 'rnattochdag' },
-  { name: 'Viktor Sarstrom', twitter: 'viktorsarstrom' },
+  { name: 'Viktor Sarström', twitter: 'viktorsarstrom' },
   { name: 'Öppna skolplattformen', twitter: 'oppnaskolplatt' },
 ]
 
