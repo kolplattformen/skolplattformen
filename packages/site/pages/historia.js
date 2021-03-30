92c7494b9ab9c99e06a660dac053844b4f0c1d23
Rickard Natt och Dag <rickard@hey.com>
2021-03-30 11:39:59 +0200

    chore: add title to history page

--- packages/site/pages/historia.js
+++ packages/site/pages/historia.js
@@ -1,10 +1,16 @@
 import Timeline from '../components/Timeline'
+import { H1 } from '../components/Typography'
 import { events } from '../data/timelineEvents'
 
 const HistoryPage = () => {
   return (
-    <section className="mx-5 md:mx-0">
-      <Timeline events={events} />
+    <section className="mx-5 max-w-2xl md:mx-auto">
+      <div className="my-8 md:my-20">
+        <H1>Media & historia</H1>
+        <div className="mt-12">
+          <Timeline events={events} />
+        </div>
+      </div>
     </section>
   )
 }
