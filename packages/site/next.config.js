c521cc0008f1920e3904f27febfe5278273236ca
Rickard Natt och Dag <rickard@hey.com>
2021-04-01 07:51:09 +0200

    chore: rewrite historia route to aktuellt

--- packages/site/next.config.js
+++ packages/site/next.config.js
@@ -2,10 +2,17 @@ const withImages = require('next-images')
 
 module.exports = {
   ...withImages(),
+  async rewrites() {
+    return [
+      {
+        source: '/historia',
+        destination: '/aktuellt',
+      },
+    ]
+  },
   i18n: {
     localeDetection: false,
     locales: ['sv', 'en'],
     defaultLocale: 'sv',
   },
 }
-
