fa9e07329a2e2ad6316d385c015f1ce9b3cf3bca
Rickard Natt och Dag <rickard@hey.com>
2021-04-01 17:52:00 +0200

    test: fix route tests

--- lib/__tests__/routes.test.ts
+++ lib/__tests__/routes.test.ts
@@ -6,13 +6,13 @@ test.each([
   ['classmates', routes.classmates('123')],
   ['classmates', routes.user],
   ['news', routes.news('123')],
-  ['newsDetails', routes.newsDetails('123')],
-  ['image', routes.image('123')],
+  ['newsDetails', routes.newsDetails('123', '321')],
+  ['image', routes.image('https://example.com/img.png')],
   ['notifications', routes.notifications('123')],
   ['menuRss', routes.menuRss('123')],
   ['menuList', routes.menuList('123')],
   ['menuChoice', routes.menuChoice('123')],
-  ['schedule', routes.schedule('123')],
+  ['schedule', routes.schedule('123', '2021-01-01', '2021-01-01')],
 ])('handles route %s', (_name, input) => {
   expect(input).toMatchSnapshot()
 })
