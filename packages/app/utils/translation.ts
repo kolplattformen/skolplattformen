144d8908d2d6ea500ab106b52df524bfa4e56799
Viktor Sarström <viktorsarstrom@gmail.com>
2021-04-13 16:47:47 +0200

    Added rtl

--- packages/app/utils/translation.ts
+++ packages/app/utils/translation.ts
@@ -1,5 +1,6 @@
 import memoize from 'fast-memoize'
 import i18n from 'i18n-js'
+import { I18nManager } from 'react-native'
 import * as RNLocalize from 'react-native-localize'
 
 const translationGetters = {
@@ -8,10 +9,13 @@ const translationGetters = {
 }
 
 export const setI18nConfig = () => {
-  const fallback = { languageTag: 'sv' }
-  const { languageTag } =
+  const fallback = { languageTag: 'sv', isRTL: false }
+
+  const { languageTag, isRTL } =
     RNLocalize.findBestAvailableLanguage(Object.keys(translationGetters)) ||
     fallback
+
+  I18nManager.forceRTL(isRTL)
 
   // @ts-expect-error Fix later
   i18n.translations = { [languageTag]: translationGetters[languageTag]() }
