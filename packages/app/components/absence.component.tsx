46c3ef2de791919613b344ef722deaf8f6a960cd
Axel <54435884+axdra@users.noreply.github.com>
2021-04-19 01:03:16 +0200

    Fixed missing translation in absence

--- packages/app/components/absence.component.tsx
+++ packages/app/components/absence.component.tsx
@@ -38,16 +38,17 @@ interface AbsenceFormValues {
   endTime: moment.Moment
 }
 
-const AbsenceSchema = Yup.object().shape({
-  socialSecurityNumber: Yup.string()
-    .required('Personnummer saknas')
-    .test('is-valid', 'Personnumret är ogiltigt', (value) =>
-      value ? Personnummer.valid(value) : true
-    ),
-  isFullDay: Yup.bool().required(),
-})
+
 
 const Absence = () => {
+  const AbsenceSchema = Yup.object().shape({
+    socialSecurityNumber: Yup.string()
+      .required(translate('abscense.personalNumberMissing'))
+      .test('is-valid', translate('abscense.invalidPersonalNumber'), (value) =>
+        value ? Personnummer.valid(value) : true
+      ),
+    isFullDay: Yup.bool().required(),
+  })
   const navigation = useNavigation<AbsenceNavigationProp>()
   const route = useRoute<AbsenceRouteProps>()
   const { sendSMS } = useSMS()
@@ -159,7 +160,7 @@ const Absence = () => {
                 {!values.isFullDay && (
                   <View style={styles.partOfDay}>
                     <View style={styles.inputHalf}>
-                      <Text style={styles.label}>Starttid</Text>
+                      <Text style={styles.label}>{translate('abscense.startTime')}</Text>
                       <Button
                         status="basic"
                         onPress={() =>
@@ -173,7 +174,9 @@ const Absence = () => {
                         confirmTextIOS={translate('general.confirm')}
                         date={moment(values.startTime).toDate()}
                         isVisible={values.displayStartTimePicker}
-                        headerTextIOS="Välj en starttid"
+                        headerTextIOS={translate(
+                          'abscense.selectAbscenseStartTime'
+                        )}
                         locale="sv-SE"
                         maximumDate={maximumDate.toDate()}
                         minimumDate={minumumDate.toDate()}
@@ -190,7 +193,7 @@ const Absence = () => {
                     </View>
                     <View style={styles.spacer} />
                     <View style={styles.inputHalf}>
-                      <Text style={styles.label}>Sluttid</Text>
+                      <Text style={styles.label}>{translate('abscense.endTime')}</Text>
                       <Button
                         status="basic"
                         onPress={() =>
