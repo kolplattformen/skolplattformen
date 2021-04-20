35e59cfa0cd41d1e1389180d72919d5f18f82e52
Elin <78786285+eahlden@users.noreply.github.com>
2021-04-20 19:20:08 +0200

    chore: update timeline (#288)
    kortat ner + lagt till polisanmälan.

--- packages/site/components/TimelineLatest.tsx
+++ packages/site/components/TimelineLatest.tsx
@@ -12,23 +12,22 @@ const TimelineLatest = () => {
           Om Öppna Skolplattformen
         </h2>
         <p>
-          Det talas numera mycket om värdet av digitalisering, innovation och
+          Det talas mycket om värdet av digitalisering, innovation och
           medskapande, om nytta för skattepengarna och medborgarinflytande.
-          Öppna skolplattformen är ett exempel på allt detta, på hur medborgare
+          Öppna skolplattformen är ett konkret exempel på detta: hur medborgare
           – i detta fall i form av en grupp frustrerade programmerande föräldrar
-          med expertis inom informationssäkerhet – med egen kraft löst ett
-          gemensamt problem till nytta för alla. Det har föranlett såväl
-          uppmuntrande tillrop från vårdnadshavare som en del medial
+          med expertis inom informationssäkerhet – med egen kraft kan lösa ett
+          gemensamt problem till nytta för alla. För det har vi fått både
+          uppmuntrande tillrop från vårdnadshavare och lärare och medial
           uppmärksamhet.
         </p>
         <p>
-          Vi förväntade oss även att vårt initiativ skulle välkomnas av
-          Stockholms stad, vars skolplattform ju fått omfattande kritik för att
+          Stockholms stads skolplattform har fått omfattande kritik för att
           den kostat enorma pengar, inte är användarvänlig och har stora
-          säkerhetsbrister. Förhoppningen var även att vi skulle kunna samarbeta
-          med staden för att skapa ännu bättre lösningar, men istället
-          attackeras nu appen från stadens sida. Här är vårt försök till att
-          skapa transparens.
+          säkerhetsbrister. Vår förväntan var att vårt initiativ skulle välkomnas och att vi skulle kunna samarbeta
+          med staden för att skapa ännu bättre lösningar. Istället
+          attackerar staden genom Utbildningsdirektör Lena Holmdahl appen och oss som privatpersoner, senast genom en polisanmälan. 
+          I ett försök att skapa transparens har vi sammanställt vad som hänt nedan.
         </p>
         <ButtonLinkInternal href="/aktuellt">
           Läs hela historien
