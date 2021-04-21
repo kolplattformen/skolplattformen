4bf571b5d8c7cb5da2453330ef22e3e4b8ae2c7d
Christian Landgren <christian@landgren.nu>
2021-04-21 23:57:07 +0200

    feat: first working version in test

--- packages/app/components/week.component.tsx
+++ packages/app/components/week.component.tsx
@@ -9,250 +9,8 @@ import {
 import React from 'react'
 import moment from 'moment'
 import { StyleSheet, View } from 'react-native'
-
-const weekNr = 15
-const lessonInfo = [
-  {
-    guidId: 'NDRhMTM1MGMtZmJmZi05MTYzLWVmNzUtMGEyZDM4Mzk0N2Q4',
-    texts: ['IDH', 'ANB', 'IdA'],
-    timeStart: '13:15:00',
-    timeEnd: '14:35:00',
-    dayOfWeekNumber: 1,
-    blockName: '',
-  },
-  {
-    guidId: 'OGZmNzQyYWEtOGJmNS05YThmLWVhZWEtMmQ1MDRhNzYxMzEw',
-    texts: ['SO', 'FEW', 'A251'],
-    timeStart: '11:10:00',
-    timeEnd: '12:20:00',
-    dayOfWeekNumber: 1,
-    blockName: '',
-  },
-  {
-    guidId: 'ODhiNjlhOGQtNGU3YS05ZjkzLWZjYWMtOWIzMTE0NGMyNDkw',
-    texts: ['NO', 'PÄA', 'A206'],
-    timeStart: '10:00:00',
-    timeEnd: '11:05:00',
-    dayOfWeekNumber: 1,
-    blockName: '',
-  },
-  {
-    guidId: 'MGExY2U3NWUtMGY3OC05NzM4LWZhY2YtNjIxOGRhMDU5NDdl',
-    texts: ['Prandium', '', ''],
-    timeStart: '12:20:00',
-    timeEnd: '12:50:00',
-    dayOfWeekNumber: 1,
-    blockName: '',
-  },
-  {
-    guidId: 'MjZjYjZjZWQtMWJhOC05M2Q1LWY2NGItZDdhZDRlMWFkNjFj',
-    texts: ['EV', 'MIZ', 'HKK'],
-    timeStart: '14:55:00',
-    timeEnd: '16:15:00',
-    dayOfWeekNumber: 1,
-    blockName: '',
-  },
-  {
-    guidId: 'NGRjOGQ1YTEtZWI1MC05ZjQ2LWMzMzQtZDVlZmU4NDMwOTBh',
-    texts: ['SL', 'HAL', 'TM'],
-    timeStart: '09:20:00',
-    timeEnd: '10:40:00',
-    dayOfWeekNumber: 2,
-    blockName: '',
-  },
-  {
-    guidId: 'Mzg5MGE3Y2MtOTA5My05NmFkLWY2MGQtYWMyNzQwZmY0MmQ0',
-    texts: ['MA', 'DAG', 'A251'],
-    timeStart: '10:45:00',
-    timeEnd: '11:40:00',
-    dayOfWeekNumber: 2,
-    blockName: '',
-  },
-  {
-    guidId: 'MDIzNjQ0MGMtYTZhZS05N2JkLWQwODQtOGQ4N2Y5OTc1NTFk',
-    texts: ['M2FR', 'STT', 'G205'],
-    timeStart: '15:05:00',
-    timeEnd: '15:55:00',
-    dayOfWeekNumber: 2,
-    blockName: '',
-  },
-  {
-    guidId: 'YTc1MTg3ODctMWVmOC05MjJmLWNjM2UtODdkMGI4NjAwMTI2',
-    texts: ['EN', 'ANL', 'A251'],
-    timeStart: '12:30:00',
-    timeEnd: '13:25:00',
-    dayOfWeekNumber: 2,
-    blockName: '',
-  },
-  {
-    guidId: 'OGM4N2ZlYjEtZjRmYi05ZGU1LWNkOWMtZDViYTc1ZTc5MWYz',
-    texts: ['NO', 'PÄA', 'A210'],
-    timeStart: '13:50:00',
-    timeEnd: '14:55:00',
-    dayOfWeekNumber: 2,
-    blockName: '',
-  },
-  {
-    guidId: 'OWFlNTNhOWQtNGIzYS05MTU1LWMzOTUtMTU3ZTBkM2VkOTE1',
-    texts: ['Prandium', '', ''],
-    timeStart: '11:40:00',
-    timeEnd: '12:10:00',
-    dayOfWeekNumber: 2,
-    blockName: '',
-  },
-  {
-    guidId: 'MmVkNGNhYzAtOTQyNC05MDhmLWNmNzgtNzllOGY2YWQ4ZGI0',
-    texts: ['NO', 'PÄA', 'A211'],
-    timeStart: '14:20:00',
-    timeEnd: '15:20:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'NDVkOTljZDEtNTc1NS05ZjY1LWM3Y2UtNzk5NjFmMDUyM2Rm',
-    texts: ['M2FR', 'STT', 'G205'],
-    timeStart: '11:20:00',
-    timeEnd: '12:10:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'YmY0ZGRhZDEtM2U2NS05NjY2LWMwYjgtMDY2N2Y1MGJhZWVj',
-    texts: ['MU', 'ANL', 'MU'],
-    timeStart: '13:10:00',
-    timeEnd: '14:10:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'YjEyY2MwZjAtNDM5MC05NDE4LWRjYzItZTMyMzU5MGZkZDIz',
-    texts: ['SV', 'MPH', 'A245'],
-    timeStart: '10:25:00',
-    timeEnd: '11:15:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'YjhmYWUyYTItNmM0OC05NWI1LWRlYWItODkwYTBkYzgzZWM0',
-    texts: ['MA', 'DAG', 'A210'],
-    timeStart: '08:15:00',
-    timeEnd: '09:10:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'ZDMxYTc4ZDYtOThiZC05NTFmLWQ0ODItNjMzNjNiODRhZDUx',
-    texts: ['SO', 'FEW', 'A251'],
-    timeStart: '09:15:00',
-    timeEnd: '10:10:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'NGEyZDRhMzUtMjlmYS05ODk1LWU0ZTAtMjNjNTM5MTVjMzFh',
-    texts: ['Prandium', '', ''],
-    timeStart: '12:20:00',
-    timeEnd: '12:50:00',
-    dayOfWeekNumber: 3,
-    blockName: '',
-  },
-  {
-    guidId: 'OWVkM2RkYzktNjUwZi05YjI1LWRhMjktMDlhMTlkOTAwOTdj',
-    texts: ['M2FR', 'STT', 'G205'],
-    timeStart: '10:10:00',
-    timeEnd: '11:00:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'MzhlZmEwMTQtNzA5Zi05ZjNlLWRhNmUtYzI1ZmMyMTJkODEz',
-    texts: ['MENTOR', 'MIZ,MAH,HAL', 'A251'],
-    timeStart: '14:10:00',
-    timeEnd: '14:40:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'OTQ1MWFiN2EtYWM4ZC05OTFkLWM5MDktMmFiNjllNTk1MzRj',
-    texts: ['SV', 'MPH', 'G305'],
-    timeStart: '08:15:00',
-    timeEnd: '09:05:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'MjFlYjFhODYtZTJlNC05MTA3LWY1YjItYjMzMjJlYjhhMGI5',
-    texts: ['Prandium', '', ''],
-    timeStart: '12:20:00',
-    timeEnd: '12:50:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'ZGIzMmVmNGQtMzgzNy05M2UxLWQ5MjItNmNlM2RiODA1YjQy',
-    texts: ['SV', 'MPH', 'G305'],
-    timeStart: '09:10:00',
-    timeEnd: '10:05:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'YjZkYzYzZDItNzFmMy05ZTc5LWRlZDQtM2E4NDA1ZWU5ZjZh',
-    texts: ['MA', 'DAG', 'A210'],
-    timeStart: '11:25:00',
-    timeEnd: '12:20:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'ZjUxMGIyYjgtNTE0YS05MmUwLWRjOWMtNDA5NjUyZDMxODU4',
-    texts: ['SO', 'FEW', 'A208'],
-    timeStart: '12:55:00',
-    timeEnd: '14:00:00',
-    dayOfWeekNumber: 4,
-    blockName: '',
-  },
-  {
-    guidId: 'M2VkOTQ2NjAtNGYwNC05MjdlLWQyM2EtNmQwNmNlMzYyNTJi',
-    texts: ['Prandium', '', ''],
-    timeStart: '12:35:00',
-    timeEnd: '13:05:00',
-    dayOfWeekNumber: 5,
-    blockName: '',
-  },
-  {
-    guidId: 'M2ZlY2M2NjctNzljMS05ZTlkLWQ2MTQtODc5NzY0ZWQ3MGJk',
-    texts: ['IDH', 'ANB', 'IdA'],
-    timeStart: '11:00:00',
-    timeEnd: '12:20:00',
-    dayOfWeekNumber: 5,
-    blockName: '',
-  },
-  {
-    guidId: 'ZGQ2OTAyZGUtZWRlNS05Mzc5LWUxMWItY2MwYmY1NGJmN2Q4',
-    texts: ['EN', 'ANL', 'A245'],
-    timeStart: '09:35:00',
-    timeEnd: '10:30:00',
-    dayOfWeekNumber: 5,
-    blockName: '',
-  },
-  {
-    guidId: 'MWNkMTUyNjQtODY0OS05OGJhLWQ0YmItZTE2YjIzNTNhZTI4',
-    texts: ['MA', 'DAG', 'A210'],
-    timeStart: '08:35:00',
-    timeEnd: '09:30:00',
-    dayOfWeekNumber: 5,
-    blockName: '',
-  },
-  {
-    guidId: 'NTlkZGMzNmEtYTEzZS05Nzg3LWQ1YTEtMjZmNWFmNDIwNmNj',
-    texts: ['HKK', 'MIZ', 'HKK'],
-    timeStart: '13:35:00',
-    timeEnd: '15:45:00',
-    dayOfWeekNumber: 5,
-    blockName: '',
-  },
-]
+import { useTimetable } from '@skolplattformen/api-hooks'
+import { TimetableEntry } from '@skolplattformen/embedded-api'
 
 const days = [
   'söndag', // yeah, they have us-type weeks in Skola24..
@@ -291,36 +49,14 @@ const subjects = {
   Prandium: 'Lunch',
 }
 
-const convert = (lessons: any) =>
+const groupOnDays = (lessons: TimetableEntry[]) =>
   lessons
-    .map(
-      ({
-        guidId: id,
-        texts: [subject, teacher, room],
-        timeStart,
-        timeEnd,
-        dayOfWeekNumber: day,
-      }) => ({
-        id,
-        subjectCode: subject,
-        subject: subjects[subject] || subject,
-        teacher,
-        room,
-        date: moment()
-          .week(weekNr)
-          .weekday(day)
-          .hours(timeStart.slice(0, 2))
-          .minute(timeStart.slice(3, 5)),
-        start: timeStart.slice(0, 5),
-        end: timeEnd.slice(0, 5),
-        day: days[day],
-      })
-    )
-    .sort((a: { date: number }, b: { date: number }) => a.date - b.date)
+    .slice()
+    .sort((a, b) => a.dateStart - b.dateStart)
     .reduce(
-      (week: { [x: string]: any }, item: { day: number }) => ({
+      (week, item) => ({
         ...week,
-        [item.day]: [...(week[item.day] || []), item],
+        [item.dayOfWeek]: [...(week[item.dayOfWeek] || []), item],
       }),
       {}
     )
@@ -334,29 +70,35 @@ const LessonList = ({ lessons, header, ...props }) => (
         {header}
       </Text>
     )}
-    renderItem={({ item: { id, subject, start, end, teacher, room } }) => (
+    renderItem={({ item: { id, code, timeStart, timeEnd, teacher, room } }) => (
       <ListItem
         key={id}
         style={styles.item}
-        title={`${subject}`}
-        description={`${start} - ${end} ${room ? `(${room})` : ''} ${teacher}`}
+        title={`${subjects[code] || code}`}
+        description={`${timeStart.slice(0,5)}-${timeEnd.slice(0,5)} ${room ? `(${room})` : ''} ${teacher}`}
       />
     )}
   />
 )
 
-export const Week = () => {
-  const data = convert(lessonInfo)
+export const Week = ({ child }) => {
   const defaultDay = moment().add(7, 'hours').weekday() // show tomorrow's schedule after 17
   const [selectedIndex, setSelectedIndex] = React.useState(defaultDay)
+  const { data: lessons } = useTimetable(
+    child,
+    moment().isoWeek(),
+    moment().year()
+  )
+  const schedule = groupOnDays(lessons)
+
   return (
     <View style={styles.view}>
       <TabBar
         selectedIndex={selectedIndex}
         onSelect={(index) => setSelectedIndex(index)}
       >
-        {Object.entries(data).map(([dayName, day]) => (
-          <Tab title={dayName} />
+        {Object.keys(schedule).map((weekDay) => (
+          <Tab key={days[weekDay]} title={days[weekDay]} />
         ))}
       </TabBar>
 
@@ -366,17 +108,17 @@ export const Week = () => {
           style={styles.pager}
           onSelect={(index) => setSelectedIndex(index)}
         >
-          {Object.entries(data).map(([dayName, day]) => (
-            <View style={styles.tab} key={dayName}>
+          {Object.entries(schedule).map(([weekDay, day]) => (
+            <View style={styles.tab} key={weekDay}>
               <View style={styles.summary}>
                 <Text category="c1" style={{ paddingBottom: 2 }}>
                   Börjar
                 </Text>
-                <Text category="h4">{day[0].start}</Text>
+                <Text category="h4">{day[0].timeStart.slice(0,5)}</Text>
                 <Text category="c1">Slutar</Text>
-                <Text category="h4">{day[day.length - 1].end}</Text>
+                <Text category="h4">{day[day.length - 1].timeEnd.slice(0,5)}</Text>
                 <Text category="c2">
-                  {day.some((lesson) => lesson.subjectCode === 'IDH')
+                  {day.some((lesson) => lesson.code === 'IDH')
                     ? '🤼‍♀️ Gympapåse'
                     : ''}
                 </Text>
@@ -385,14 +127,14 @@ export const Week = () => {
                 style={styles.part}
                 header="FM"
                 lessons={day
-                  .filter(({ start }) => start <= '12:00')
+                  .filter(({ timeStart }) => timeStart < '12:00')
                   .sort((a, b) => a.date - b.date)}
               />
               <LessonList
                 style={styles.part}
                 header="EM"
                 lessons={day
-                  .filter(({ start }) => start >= '12:00')
+                  .filter(({ timeStart }) => timeStart >= '12:00')
                   .sort((a, b) => a.date - b.date)}
               />
             </View>
