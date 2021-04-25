62e61430af89ed32d5e190a381355b4d2993de1c
David Fant <david@fant.io>
2021-04-25 18:38:18 +0200

    fuzzy search news

--- packages/app/components/newsList.component.tsx
+++ packages/app/components/newsList.component.tsx
@@ -1,25 +1,66 @@
+import React, { useState, useMemo } from 'react'
 import { useNews } from '@skolplattformen/api-hooks'
-import { List } from '@ui-kitten/components'
-import React from 'react'
+import { List, Input } from '@ui-kitten/components'
 import { StyleSheet } from 'react-native'
 import { useChild } from './childContext.component'
 import { NewsListItem } from './newsListItem.component'
+import { translate } from '../utils/translation'
+import {
+  useNewsListSearchResults,
+  renderSearchResultPreview,
+} from '../utils/search'
+import { SearchIcon } from './icon.component'
 
 export const NewsList = () => {
   const child = useChild()
   const { data } = useNews(child)
 
-  return (
-    <>
+  const [searchQuery, setSearchQuery] = useState('')
+  const searchResults = useNewsListSearchResults(searchQuery)
+
+  const header = useMemo(
+    () => (
+      <Input
+        accessibilityLabel={translate('news.search.placeholder')}
+        placeholder={translate('news.search.placeholder')}
+        returnKeyType="search"
+        status="basic"
+        accessoryLeft={SearchIcon}
+        onChangeText={setSearchQuery}
+        value={searchQuery}
+      />
+    ),
+    [searchQuery]
+  )
+
+  if (searchQuery) {
+    return (
       <List
         style={styles.container}
         contentContainerStyle={styles.contentContainer}
-        data={data}
-        renderItem={(info) => (
-          <NewsListItem key={info.item.id} item={info.item} />
+        keyboardDismissMode="on-drag"
+        data={searchResults}
+        ListHeaderComponent={header}
+        renderItem={({ item: searchResult }) => (
+          <NewsListItem key={searchResult.item.id} item={searchResult.item}>
+            {renderSearchResultPreview(searchResult)}
+          </NewsListItem>
         )}
       />
-    </>
+    )
+  }
+
+  return (
+    <List
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      keyboardDismissMode="on-drag"
+      data={data}
+      ListHeaderComponent={header}
+      renderItem={(info) => (
+        <NewsListItem key={info.item.id} item={info.item} />
+      )}
+    />
   )
 }
 
