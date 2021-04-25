8b700e5516414d4e0c4e70a2ff64a245645d1cd3
Christian Landgren <christian@landgren.nu>
2021-04-25 13:54:18 +0200

    feat: use curriculun lib

--- packages/app/components/week.component.tsx
+++ packages/app/components/week.component.tsx
@@ -10,6 +10,7 @@ import React from 'react'
 import moment from 'moment'
 import { StyleSheet, View } from 'react-native'
 import { useTimetable } from '@skolplattformen/api-hooks'
+import parse from '@skolplattformen/curriculum'
 import { TimetableEntry } from '@skolplattformen/embedded-api'
 
 const days = [
@@ -22,39 +23,14 @@ const days = [
   'lördag',
 ]
 
-const subjects = {
-  BL: 'Bild',
-  BI: 'Biologi',
-  EN: 'Engelska',
-  EV: 'Elevens val', // jag gissar
-  FY: 'Fysik',
-  GE: 'Geografi',
-  HKK: 'Hemkunskap',
-  HI: 'Historia',
-  IDH: 'Idrott och hälsa',
-  KE: 'Kemi',
-  MA: 'Matematik',
-  M1: 'Moderna språk', // will have extra language code
-  M2: 'Moderna språk', // will have extra language code
-  ML: 'Modersmål', // will have extra language code
-  M2FR: 'Franska',
-  MU: 'Musik',
-  RE: 'Religionskunskap',
-  SH: 'Samhällskunskap',
-  SL: 'Slöjd',
-  SV: 'Svenska',
-  SVA: 'Svenska som andraspråk',
-  TN: 'Teckenspråk för hörande',
-  TK: 'Teknik',
-  Prandium: 'Lunch',
-}
-
 const groupOnDays = (lessons: TimetableEntry[]) =>
   lessons
     .slice()
-    .sort((a, b) => a.dateStart - b.dateStart)
+    .sort((a: TimetableEntry, b: TimetableEntry) =>
+      a.dateStart.localeCompare(b.dateStart)
+    )
     .reduce(
-      (week, item) => ({
+      (week: Object, item: TimetableEntry) => ({
         ...week,
         [item.dayOfWeek]: [...(week[item.dayOfWeek] || []), item],
       }),
@@ -74,16 +50,17 @@ const LessonList = ({ lessons, header, ...props }) => (
       <ListItem
         key={id}
         style={styles.item}
-        title={`${subjects[code] || code}`}
-        description={`${timeStart.slice(0,5)}-${timeEnd.slice(0,5)} ${room ? `(${room})` : ''} ${teacher}`}
+        title={`${parse(code).name || code}`}
+        description={`${timeStart.slice(0, 5)}-${timeEnd.slice(0, 5)} ${
+          room ? `(${room})` : ''
+        } ${teacher}`}
       />
     )}
   />
 )
 
 export const Week = ({ child }) => {
-  const defaultDay = moment().add(7, 'hours').weekday() // show tomorrow's schedule after 17
-  const [selectedIndex, setSelectedIndex] = React.useState(defaultDay)
+  const [selectedIndex, setSelectedIndex] = React.useState(0)
   const { data: lessons } = useTimetable(
     child,
     moment().isoWeek(),
@@ -114,9 +91,11 @@ export const Week = ({ child }) => {
                 <Text category="c1" style={{ paddingBottom: 2 }}>
                   Börjar
                 </Text>
-                <Text category="h4">{day[0].timeStart.slice(0,5)}</Text>
+                <Text category="h4">{day[0].timeStart.slice(0, 5)}</Text>
                 <Text category="c1">Slutar</Text>
-                <Text category="h4">{day[day.length - 1].timeEnd.slice(0,5)}</Text>
+                <Text category="h4">
+                  {day[day.length - 1].timeEnd.slice(0, 5)}
+                </Text>
                 <Text category="c2">
                   {day.some((lesson) => lesson.code === 'IDH')
                     ? '🤼‍♀️ Gympapåse'
