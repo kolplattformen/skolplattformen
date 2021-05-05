7e251b6ab95d80a0c69fbdfab766ec3dac190580
Viktor Sarström <viktorsarstrom@gmail.com>
2021-05-05 21:01:34 +0200

    readded border color

--- packages/app/components/newsListItem.component.tsx
+++ packages/app/components/newsListItem.component.tsx
@@ -71,6 +71,7 @@ const themedStyles = StyleService.create({
     marginBottom: Sizing.t2,
 
     backgroundColor: 'background-basic-color-1',
+    borderColor: 'border-basic-color-3',
   },
   text: {
     ...Layout.flex.full,
