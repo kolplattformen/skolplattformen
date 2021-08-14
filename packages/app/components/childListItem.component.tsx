d58d1a9b709a3ef14ff252a0cffa9eb948d59b12
Christian Landgren <christian@landgren.nu>
2021-08-14 12:59:12 +0200

    chore: fix linting problems

--- packages/app/components/childListItem.component.tsx
+++ packages/app/components/childListItem.component.tsx
@@ -22,7 +22,6 @@ import { studentName } from '../utils/peopleHelpers'
 import { translate } from '../utils/translation'
 import { RootStackParamList } from './navigation.component'
 import { StudentAvatar } from './studentAvatar.component'
-import { DaySummary } from './daySummary.component'
 
 interface ChildListItemProps {
   child: Child
@@ -95,7 +94,6 @@ export const ChildListItem = ({ child, color }: ChildListItemProps) => {
 
   const className = getClassName()
   const styles = useStyleSheet(themeStyles)
-  const date = moment()
 
   return (
     <TouchableOpacity
--- packages/app/index.js
+++ packages/app/index.js
@@ -6,5 +6,4 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-
 AppRegistry.registerComponent(appName, () => App)
