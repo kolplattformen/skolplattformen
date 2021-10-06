5fdc3d71adb8d9364c8c6b345de841f0f3f1de7c
Jonathan Edenström <jonathan@edenstrom.io>
2021-10-06 07:09:54 +0200

    fix: ui-kitten metro config

--- apps/skolplattformen-sthlm/metro.config.js
+++ apps/skolplattformen-sthlm/metro.config.js
@@ -3,34 +3,24 @@ const { withNxMetro } = require('@nrwl/react-native')
 const MetroConfig = require('@ui-kitten/metro-config')
 const evaConfig = {
   evaPackage: '@eva-design/eva',
-  customMappingPath: './design/mapping.json',
+  customMappingPath: './apps/skolplattformen-sthlm/design/mapping.json',
 }
 
-// const evaMetroConfig = MetroConfig.create(evaConfig, {transformer: {
-//       getTransformOptions: async () => ({
-//         transform: {
-//           experimentalImportSupport: false,
-//           inlineRequires: true,
-//         },
-//       }),
-//     },})
-
-module.exports = withNxMetro(
-  {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: true,
-        },
-      }),
-    },
+const evaMetroConfig = MetroConfig.create(evaConfig, {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
-  {
-    // Change this to true to see debugging info.
-    // Useful if you have issues resolving modules
-    debug: false,
-    // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx'
-    extensions: [],
-  }
-)
+})
+
+module.exports = withNxMetro(evaMetroConfig, {
+  // Change this to true to see debugging info.
+  // Useful if you have issues resolving modules
+  debug: false,
+  // all the file extensions used for imports other than 'ts', 'tsx', 'js', 'jsx'
+  extensions: [],
+})
