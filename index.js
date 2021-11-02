002e8c13205c65c4b7a6823b7e4c05728710fbba
Viktor Sarström <viktorsarstrom@gmail.com>
2021-11-02 10:22:50 +0100

    Added index.js to root

---
+++ index.js
@@ -0,0 +1,6 @@
+import 'react-native-gesture-handler'
+import { AppRegistry } from 'react-native'
+import App from './apps/skolplattformen-sthlm/App'
+import { name as appName } from './apps/skolplattformen-sthlm/app.json'
+
+AppRegistry.registerComponent(appName, () => App)
