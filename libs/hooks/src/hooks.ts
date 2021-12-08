34db8682f1206c595937dc2b6461b3a6c3f3eef1
Kajetan Kazimierczak <kajetan@hotmail.com>
2021-12-08 23:41:26 +0100

    feat: 🎸 Teacher's names in timetable (where available)

--- libs/hooks/src/hooks.ts
+++ libs/hooks/src/hooks.ts
@@ -222,7 +222,7 @@ export const useTeachers = (child: Child) =>
   )
 
 export const useTimetable = (
-  child: Skola24Child,
+  child: Child,
   week: number,
   year: number,
   lang: Language
@@ -232,8 +232,30 @@ export const useTimetable = (
     `timetable_${child.personGuid}_${week}_${year}_${lang}`,
     [],
     (s) => s.timetable,
-    (api) => () => api.getTimetable(child, week, year, lang)
+    (api) => async () => {
+      const tt = await api.getTimetable(child, week, year, lang)
+      const ts = await api.getTeachers(child)
+      tt.forEach((element) => {
+        element.teacher = replaceTeacherInitials(element.teacher, ts)
+      })
+      return tt
+    }
   )
+
+const replaceTeacherInitials = (
+  initials: string,
+  teachers: Teacher[]
+): string => {
+  if (!initials || teachers?.length == 0) return initials
+  const arr = initials.split(',') || [initials]
+  const arr2 = arr.map((element) => {
+    const t = teachers.find(
+      (t) => t.timeTableAbbreviation === element.trim().toUpperCase()
+    )
+    return t ? `${t.firstname} ${t.lastname}` : element
+  })
+  return arr2.join(', ')
+}
 
 export const useUser = () =>
   hook<User>(
