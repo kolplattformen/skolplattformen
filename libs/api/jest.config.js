e50b991f54443beaaf9d4390f6efe0ff111ca033
Jonathan Edenström <jonathan@edenstrom.io>
2021-12-16 11:52:21 +0100

    fix: change jest config for api's

--- libs/api/jest.config.js
+++ libs/api/jest.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   displayName: 'api',
-  preset: 'react-native',
   resolver: '@nrwl/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
   setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
-  transform: {
-    '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
-    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
-      'react-native/jest/assetFileTransformer.js'
-    ),
-  },
 }
