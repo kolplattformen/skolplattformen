e2920e878dde79e8a2a08ee4a89e44158e26b533
Palmn72 <cs.palmqvist@gmail.com>
2023-10-20 15:04:17 +0200

    fix(changed file-ending to pass provider.test)
    Co-authored-by: Lee Sheppard<coolusername244@users.noreply.github.com>

--- apps/skolplattformen-app-new/libs/hooks/src/__tests__/fake.test.tsx
+++ apps/skolplattformen-app-new/libs/hooks/src/__tests__/fake.test.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook, waitFor } from '@testing-library/react'
+import { renderHook, waitFor } from '@testing-library/react'
 import React from 'react'
 import { ApiProvider } from '../provider'
 import {
