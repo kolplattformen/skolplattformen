84ddda3f4636d2a5b27913e77cc3169ae9cc9cdc
Kajetan Kazimierczak <kajetan-kazimierczak@users.noreply.github.com>
2024-02-13 08:52:03 +0100

    Update routes.ts

--- libs/api-skolplattformen/lib/routes.ts
+++ libs/api-skolplattformen/lib/routes.ts
@@ -22,7 +22,7 @@ export const frejaReturnUrl =
 export const frejaLoginCookie =
   'https://login003.stockholm.se/NECSadcfreja/authenticate/SiteMinderAuthADCFREJA?TYPE=33554433&REALMOID=06-89cf916c-9764-45fa-8690-eaf3fe9282bc&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=IfNE0iMOtzq2TcxFADHylR6rkmFtwzoxRKh5nRMO9NBqIxHrc38jFyt56FASdxk1&TARGET=-SM-HTTPS%3a%2f%2flogin001%2estockholm%2ese%2fNECSadc%2ffreja%2fb64startpage%2ejsp%3fstartpage%3daHR0cHM6Ly9ldGphbnN0LnN0b2NraG9sbS5zZS92YXJkbmFkc2hhdmFyZS9pbmxvZ2dhZDIvT3ZlcnNpa3Q%3d'
 
-const urlLoggedIn = `https://etjanst.stockholm.se/vardnadshavare/inloggad2`
+const urlLoggedIn = `https://etjanster.stockholm.se/vardnadshavare/inloggad2`
 
 export const children = `${urlLoggedIn}/GetChildren`
 
@@ -39,7 +39,7 @@ export const schoolContacts = (childId: string, schoolId: string) =>
   `${urlLoggedIn}/contacts/GetSchoolContacts?schoolId=${schoolId}&studentId=${childId}&schoolForm=Klasslista`
 
 export const user =
-  'https://etjanst.stockholm.se/vardnadshavare/base/getuserdata'
+  'https://etjanster.stockholm.se/vardnadshavare/base/getuserdata'
 
 export const news = (childId: string) =>
   `${urlLoggedIn}/News/GetNewsArchive?bannerImageLimit=5000&childId=${childId}`
@@ -64,22 +64,22 @@ export const menuChoice = (childId: string) =>
 export const schedule = (childId: string, fromDate: string, endDate: string) =>
   `${urlLoggedIn}/Calender/GetSchema?childId=${childId}&startDate=${fromDate}&endDate=${endDate}`
 
-export const cdn = 'https://etjanst.stockholm.se/vardnadshavare/base/cdn'
+export const cdn = 'https://etjanster.stockholm.se/vardnadshavare/base/cdn'
 
-export const auth = 'https://etjanst.stockholm.se/vardnadshavare/base/auth'
+export const auth = 'https://etjanster.stockholm.se/vardnadshavare/base/auth'
 
 export const startBundle =
-  'https://etjanst.stockholm.se/vardnadshavare/bundles/start'
+  'https://etjanster.stockholm.se/vardnadshavare/bundles/start'
 
 export const hemPage =
-  'https://etjanst.stockholm.se/vardnadshavare/inloggad2/hem'
+  'https://etjanster.stockholm.se/vardnadshavare/inloggad2/hem'
 
 export const navigationControllerScript =
-  'https://etjanst.stockholm.se/vardnadshavare/bundles/navigationController'
+  'https://etjanster.stockholm.se/vardnadshavare/bundles/navigationController'
 
-export const baseEtjanst = 'https://etjanst.stockholm.se'
+export const baseEtjanst = 'https://etjanster.stockholm.se'
 
-export const childcontrollerScript = `https://etjanst.stockholm.se/vardnadshavare/bundles/childcontroller?v=${Date.now()}`
+export const childcontrollerScript = `https://etjanster.stockholm.se/vardnadshavare/bundles/childcontroller?v=${Date.now()}`
 
 export const createItemConfig =
   'https://raw.githubusercontent.com/kolplattformen/embedded-api/main/config.json'
@@ -103,4 +103,4 @@ export const topologyConfigUrl =
   'https://fantomenkrypto.vercel.app/api/getConfig'
 
 export const selectChild =
-  'https://etjanst.stockholm.se/vardnadshavare/inloggad2/SelectChild'
+  'https://etjanster.stockholm.se/vardnadshavare/inloggad2/SelectChild'
