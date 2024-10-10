4f7c2de54c95661081d68d576004fc84d2a24205
starkodder <74370377+starkodder@users.noreply.github.com>
2024-10-10 07:48:23 +0200

    Delete libs/api-hjarntorget directory

--- libs/api-hjarntorget/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  displayName: 'api-hjarntorget',
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
-  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
-}
