4b0d57ae5cdf027c44601ca75d9d309810944992
Christian Landgren <christian@landgren.nu>
2026-09-06 10:05:42 +0200

    test(curriculum): ersätt borttagen RN-preprocessor
    jest.config krävde react-native/jest/preprocessor.js som inte längre
    finns i RN 0.76. curriculum är ett rent TS-parsningsbibliotek, så
    konfigen är nu densamma som övriga api-libs: node-miljö + ts-jest mot
    tsconfig.spec.json. Den döda test-setup.ts (jest-native-import) togs
    bort när inget längre refererar den.

--- libs/curriculum/jest.config.js
+++ libs/curriculum/jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   displayName: 'curriculum',
-  preset: 'react-native',
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html', 'tsx', 'jsx'],
-  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  testEnvironment: 'node',
   transform: {
-    '\\.(js|ts|tsx)$': require.resolve('react-native/jest/preprocessor.js'),
-    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': require.resolve(
-      'react-native/jest/assetFileTransformer.js'
-    ),
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
-};
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/curriculum',
+}
